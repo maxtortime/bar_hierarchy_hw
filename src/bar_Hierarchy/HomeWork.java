335b1da226e5295aa94ed5dfc243cba8b06d8fd5
maxtortime <maxtortime@gmail.com>
2015-04-11 15:45:28 +0900

    json data input test

--- src/bar_Hierarchy/HomeWork.java
+++ src/bar_Hierarchy/HomeWork.java
@@ -4,8 +4,7 @@
 package bar_Hierarchy;
 
 import processing.core.PApplet;
-import org.apache.commons.lang3.*;
-import java.awt.Color;
+
 /**
  * @author Taehwan
  *
@@ -13,15 +12,7 @@ import java.awt.Color;
 @SuppressWarnings("serial")
 
 public class HomeWork extends PApplet
-{
-	public HomeWork() {
-		int m[] = new int[]{80,160,0,160}; // top right bottom left
-		int w = 1280- m[1] - m[3]; // width
-		int h = 800 - m[0] - m[2]; // height
-		Range<Integer> x = Range.between(0, w);
-		Range<Integer> z = Range.between(0x4682B4, 0xAAAAAA);
-	}
-	
+{	
 	public void setup() 
 	{
 		size(960,640);
@@ -30,12 +21,7 @@ public class HomeWork extends PApplet
 	
 	public void draw()
 	{
-		beginShape();
-		vertex(30, 20);
-		vertex(85, 20);
-		vertex(85, 75);
-		vertex(30, 75);
-		endShape(CLOSE);
+		rect(5,5,(float) 393.8,20);
 	}
 	
 	
