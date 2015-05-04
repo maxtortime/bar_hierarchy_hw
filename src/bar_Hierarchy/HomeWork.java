0d40077e2cf20d9705d842e8d17787b9bebd8bd9
maxtortime <maxtortime@gmail.com>
2015-05-05 00:03:21 +0900

    바를 클릭할 수 있는 상태로 만들어놓음

--- src/bar_Hierarchy/HomeWork.java
+++ src/bar_Hierarchy/HomeWork.java
@@ -3,7 +3,7 @@
  */
 package bar_Hierarchy;
 
-import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.TreeMap;
 import java.util.Vector;
@@ -25,69 +25,90 @@ import processing.core.PApplet;
 public class HomeWork extends PApplet
 {	
 	static Vector<Integer> sum = new Vector<Integer>();
-	static TreeMap<Integer,String> sumTree = new TreeMap<Integer,String>(Collections.reverseOrder());
-	static TreeMap<Integer,JSONObject> objTree = new TreeMap<Integer,JSONObject>();
+	static HashMap<String,Integer> sumTree = new HashMap<String,Integer>();
+	static TreeMap<String, JSONObject> objTree = new TreeMap<String,JSONObject>();
+	static ValueComparator bvc = new ValueComparator(sumTree);
+	static TreeMap<String,Integer> sorted_map = new TreeMap<String, Integer>(bvc);
+	
+	boolean rectOver,buttonOver;
+	int rectHighlight, buttonHighlight;
+	int rectX;
+	Vector<Integer> rectY = new Vector<Integer>();
+	Vector<Integer> rectWidth = new Vector<Integer>();
+	Vector<String> rectName = new Vector<String>();
+	int rectHeight;
+	int rectColor;
+	int baseColor;
+	int currentColor;
+	
+	JSONArray depth1;
 	
 	public void setup()
 	{
 		size(640,480);
+		textSize(12);
+
+		rectX = 80;
+		rectY.add(0);
+		rectHeight = 32;
+		rectHighlight = color(0,0,255);
+		rectColor = color(0,0,0);
+		rectOver = false;
+		
+		baseColor = color(245);
+		currentColor = baseColor;
+		
 		String s = FTFile.Read("../flare.json");
 		JSONObject obj;
 		
 		try {
 			obj = new JSONObject(s);
 		
-			JSONArray depth1 = obj.getJSONArray("children").getJSONObject(0).getJSONArray("children");
+			depth1 = obj.getJSONArray("children");
 			
 			for (int i = 0; i < depth1.length() ; i++) {
 				FlareData d = new FlareData(depth1.getJSONObject(i));
-				//sumTree.put(d.sum(), d.getObj().getString("name"));
-				objTree.put(d.sum(), d.getObj());
+				sumTree.put(d.getObj().getString("name"),d.sum());
+				objTree.put(d.getObj().getString("name"),d.getObj());
 			}
 		} catch (JSONException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		
+		sorted_map.putAll(sumTree);
+		
+		for (Entry<String, Integer> entry : sorted_map.entrySet()) {
+			rectWidth.add((int) map(entry.getValue(),0,500000,10,400));
+			rectName.add(entry.getKey());
+			
+			rectY.add(rectY.lastElement()+40);
+		}
+		
+		noLoop();
 	}
 
 	public void draw()
 	{
-		int y=0;
 		
-		for(Entry<Integer, JSONObject> entry : objTree.entrySet())
-		{  
-			textSize(12);
-		
-			if(mouseY>y && mouseY<=y+40) { 
-				fill(0,0,255);
-				try {
-					for (int i = 0 ; i<entry.getValue().getJSONArray("children").length() ; i++) {
-						FlareData d2 = new FlareData(entry.getValue().getJSONArray("children").getJSONObject(i));
-											}
-				} catch (JSONException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-				// 다음 depth ㅢ 합계 출력
+		for (int i = 0 ; i < sorted_map.size() ; i++) {
+			if(mouseY>rectY.get(i) && mouseY<=rectY.get(i)+40 && mouseX <= rectWidth.get(i)) {
+				
+				fill(255,40,40);
 			}
 			else {
 				fill(50);
-				// 이전 depth의 합계 출력
 			}
-				
+			rect(rectX,rectY.get(i)+10,rectWidth.get(i),rectHeight);
+			text(rectName.get(i),10,rectY.get(i)+25);
+		}
+	}
 	
-		    float w = map(entry.getKey(), 0,500000,10,400);
-
-		    rect(80,y+10,w,32);
-		    
-		    try {
-				text(entry.getValue().getString("name"),10,y+25);
-			} catch (JSONException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		     
-		    y+=40;
-		}  
-	}	
+	public void mousePressed() {
+		  loop();  // Holding down the mouse activates looping
+		}
+	
+	public void mouseReleased() {
+		  noLoop();  // Releasing the mouse stops looping draw()
+		}
 }
